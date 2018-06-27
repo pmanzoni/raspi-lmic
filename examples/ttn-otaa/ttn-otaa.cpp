11acca7e405888685629d5d69b027863dc5bdf7f
pmanzoni <manzoni@ieee.org>
2018-06-27 18:48:41 +0200

    Emptied device and app data

--- examples/ttn-otaa/ttn-otaa.cpp
+++ examples/ttn-otaa/ttn-otaa.cpp
@@ -40,11 +40,11 @@
 // This EUI must be in little-endian format, so least-significant-byte
 // first. When copying an EUI from ttnctl output, this means to reverse
 // the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,0x70.
-static const u1_t PROGMEM APPEUI[8]= { 0xB1, 0x01, 0x01, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 };
+static const u1_t PROGMEM APPEUI[8]= { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
 
 // This should also be in little endian format, see above.
-static const u1_t PROGMEM DEVEUI[8]= { 0x00, 0x04, 0x2A, 0x09, 0xA1, 0xEB, 0x27, 0xB8 };
+static const u1_t PROGMEM DEVEUI[8]= { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 // Here on Raspi we use part of MAC Address do define devEUI so 
 // This one above is not used, but you can still old method 
 // reverting the comments on the 2 following line
@@ -55,7 +55,7 @@ void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
 // number but a block of memory, endianness does not really apply). In
 // practice, a key taken from ttnctl can be copied as-is.
 // The key shown here is the semtech default key.
-static const u1_t PROGMEM APPKEY[16] = { 0x78, 0x62, 0x9C, 0x05, 0x91, 0x8D, 0xC4, 0x35, 0xB2, 0xB6, 0x20, 0xCC, 0x5B, 0x8D, 0xB8, 0x1C };
+static const u1_t PROGMEM APPKEY[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
 
 static uint8_t mydata[] = "Raspi TESTING!";
